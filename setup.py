3281000a447935f3a93fd6558e6e3656db947bdb
Chris Conlin <cconlin@gmail.com>
2010-12-13 20:49:01 -0500

    Made README GitHub friendly

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     version = '0.1',
     description = 'Beaker backend to write sessions and caches to a ' +\
     'MongoDB schemaless database.',
-    long_description = '\n' + open('README').read(),
+    long_description = '\n' + open('README.rst').read(),
     author='Brendan W. McAdams',
     author_email = 'bwmcadams@gmail.com',
     keywords = 'mongo mongodb beaker cache session',
